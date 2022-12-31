9e45fc81c734e0cbf23fa3e7ff0083dc08e4fb59
kennedyisn <kennedyhayles@gmail.com>
2022-12-31 14:19:51 -0500

    Password generator criteria statements

--- script.js
+++ script.js
@@ -1,5 +1,67 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "@", "[", "`", "{", "|", "}", "~"];
+var numSet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var uppercaseAlph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseAlph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// placeholder for selections made by the user to create their password
+var passwordCriteria = []
+
+function createPassword() {
+  // Password length prompt
+  var passwordLengthQ = Number(prompt("How long would you like your password to be? Please select a number between 8 and 128."));
+  console.log(passwordLengthQ);
+
+  // Parsing input from prompt to make sure it is read as a number
+  var passwordLength = parseInt(passwordLengthQ)
+
+  if (passwordLength <= 7 || passwordLength >= 129) {
+    Number(prompt("Please enter a number between 8 and 128."))
+  } 
+	else {
+    console.log('User would like the password length to be' + passwordLength)
+  }
+
+  // Lowercase font prompt
+  var lowerCase = window.confirm ("Would you like your password to contain lowercase characters?")
+  console.log('User would like to include lowercase letters-' + lowerCase)
+  
+  // Uppercase font prompt
+  var upperCase = window.confirm ("Would you like your password to contain uppercase characters?")
+  console.log('User would like to include uppercase letters-' + upperCase)
+  
+  // Special characters prompt
+  var specChar = window.confirm ("Would you like your password to contain special characters(i.e !, #, $, %, &)?")
+  console.log('User would like to include Special Characters' + specChar)
+
+  // Numbers prompt
+  var numberSelec = window.confirm ("Would you like your password to include numbers?")
+  console.log('User would like to include numbers' + numberSelec)
+
+  // Boolean if no criteria are selected then the user will be alerted to choose atleast one
+  if (!lowerCase && !upperCase && !specChar && !numberSelec) {
+    alert('Please choose atleast one criteria for your password.')
+  }
+
+  // if statement using concat to add the selected strings made at the beginning of the code together to make a list of characters fitting user criteria
+  if (lowerCase) {
+    passwordCriteria = passwordCriteria.concat(lowercaseAlph)
+  }
+  if (upperCase) {
+    passwordCriteria = passwordCriteria.concat(uppercaseAlph)
+  }
+  if (specChar) {
+    passwordCriteria = passwordCriteria.concat(specialChar)
+  }
+  if (numberSelec) {
+    passwordCriteria = passwordCriteria.concat(numSet)
+  };
+  console.log(passwordCriteria);
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +69,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
